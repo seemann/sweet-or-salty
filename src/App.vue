2f71bfd3a76a788f6311aa8356782caa7304f5e4
Manuel Seemann <manuel@work.de>
2016-02-24 14:14:55 +0100

    Add some styles

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <template>
-  <div id="app">
+  <div id="app" class="px4">
     <h1>Sweet or Salty</h1>
-    <search></search>
-    <results></results>
+    <div class="clearfix mxn1">
+      <div class="col col-3 px1">
+        <search></search>
+        <results></results>
+      </div>
+      <div class="col col-9 px1">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
-  body {
-    font-family: Helvetica, sans-serif;
-  }
+<style lang="sass">
+  @import '/components/normalize/normalize';
+  @import '/components/variables/variables';
+  @import '/components/functions/functions';
+  @import '/components/layout/layout';
+  @import '/components/margin/margin';
+  @import '/components/padding/padding';
+  @import '/components/base/base';
+  @import '/components/typography/typography';
+  @import '/components/grid/grid';
 </style>
 
 <script>
-  import Search from './components/Search'
-  import Results from './components/Results'
+  import Search from './components/search/Search'
+  import Results from './components/results/Results'
 
   export default {
     components: {
