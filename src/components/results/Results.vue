14bb3c9ae4a92c19a22f20885aa7e27f43815009
Manuel Seemann <manuel@work.de>
2016-02-24 14:14:29 +0100

    Add some styles

---
+++ src/components/results/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="results list-reset">
+    <li v-for="result in results" v-if="result.type === 'show'">
+      <img v-bind:src="result.show.images.poster.thumb" alt="" />
+      <h3>{{ result.show.title }} <small>{{ result.type }}</small></h3>
+    </li>
+
+    <li v-for="result in results" v-if="result.type === 'movie'">
+      <img v-bind:src="result.movie.images.poster.thumb" alt="" />
+      <h3>{{ result.movie.title }} <small>{{ result.type }}</small></h3>
+    </li>
+  </ul>
+</template>
+
+<style lang="sass" scoped>
+  @import '../variables/variables';
+  @import '../functions/functions';
+  @import '../margin/margin';
+  @import '../lists/lists';
+</style>
+
+<script>
+  import store from './../../store'
+
+  export default {
+    computed: {
+      results () {
+        return store.state.results
+      }
+    }
+  }
+</script>
---
+++ src/components/search/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <form class="search" @submit.prevent>
+    <label for="search">Search</label>
+    <input :value="searchQuery" @keyup.enter="search" id="search" type="search">
+  </form>
+</template>
+
+<style lang="sass" scoped>
+  @import '../variables/variables';
+  @import '../functions/functions';
+  @import '../margin/margin';
+  @import '../padding/padding';
+  @import '../forms/forms';
+</style>
+
+<script>
+  import store from './../../store'
+
+  export default {
+    methods: {
+      search: function (event) {
+        store.actions.search(event.target.value)
+      }
+    }
+  }
+</script>
