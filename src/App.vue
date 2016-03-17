bc29ca353442133d54b5f7e83f11d94d973b221a
Manuel Seemann <manuel@work.de>
2016-03-17 15:17:33 +0100

    Change transitions

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <template>
-  <div id="app" class="px4">
+  <div id="app" class="mx4">
     <h1>Sweet or Salty</h1>
-    <div class="clearfix mxn1">
-      <div class="col col-3 px1">
-        <search></search>
-        <results></results>
-      </div>
-      <div class="col col-9 px1">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </div>
-    </div>
+    <search></search>
+    <results></results>
   </div>
 </template>
 
@@ -17,7 +10,7 @@
   @import '/components/normalize/normalize';
   @import '/components/variables/variables';
   @import '/components/functions/functions';
-  @import '../node_modules/animate.css/animate.min.css';
+  @import '/components/transitions/transitions';
   @import '/components/layout/layout';
   @import '/components/margin/margin';
   @import '/components/padding/padding';
--- src/main.js
+++ src/main.js
@@ -10,14 +10,15 @@ Vue.http.headers.common['Content-type'] = 'application/json'
 Vue.http.headers.common['trakt-api-version'] = '2'
 Vue.http.headers.common['trakt-api-key'] = '6ec6cc9ee686c3c6dd74e4569b2a64e10d97c5e523a769e3266f8ae60526fc91'
 
+Vue.transition('slide', {
+  enterClass: 'slideIn',
+  leaveClass: 'slideOutDown'
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
   components: {
     App
   }
-})
-
-Vue.transition('bounce', {
-  enterClass: 'bounceInDown'
 })
