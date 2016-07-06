0f02bcc4241fa04e86eca6b73609374af3f0a643
Manuel Seemann <manuel@work.de>
2016-07-06 15:36:58 +0200

    Fresh start

--- src/actions.js
+++
@@ -1,6 +0,0 @@
-export const search = makeAction('SEARCH')
-export const detail = makeAction('DETAIL')
-
-function makeAction (type) {
-  return ({ dispatch }, ...args) => dispatch(type, ...args)
-}
