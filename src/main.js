3678d709f8b759f8799e83377d186767d8658d72
Manuel Seemann <manuel@work.de>
2016-07-08 16:07:42 +0200

    Update API URL

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App.vue';
 Vue.use(VueResource);
 
 /* Set global Vue resource options */
-Vue.http.options.root = 'https://api-v2launch.trakt.tv';
+Vue.http.options.root = 'https://api.trakt.tv';
 Vue.http.headers.common['Content-type'] = 'application/json';
 Vue.http.headers.common['trakt-api-version'] = '2';
 Vue.http.headers.common['trakt-api-key'] = '6ec6cc9ee686c3c6dd74e4569b2a64e10d97c5e523a769e3266f8ae60526fc91';
