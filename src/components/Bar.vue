de2f71ba322fa0f914199f38398d12c7903649e7
accounts@manuelseemann.de <tnsCZHPnpomqd7g8v>
2016-07-10 00:25:42 +0200

    Clean up

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -1,17 +1,17 @@
 <template>
   <header class="bar">
-    <h1 class="bar__title">Sweet Or Salty</h1>
+    <div class="container">
+      <h1 class="bar__title">Sweet Or Salty</h1>
+    </div>
   </header>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="sass" scoped>
-  @import '../global';
+  @import '../variables';
 
   .bar {
     font-size: 1rem;
