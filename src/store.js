fe237cee8f7350eec8e385010832ac9b25aef3d1
Manuel Seemann <manuel@work.de>
2016-07-11 09:55:26 +0200

    Remove getSeasons fromstore

--- src/store.js
+++ src/store.js
@@ -10,15 +10,7 @@ module.exports = {
     Vue.http.get(`search/show?query=${query}&extended=images`).then(response => {
       this.state.results = response.json();
     }, response => {
-      console.error(response.statusText);
-    });
-  },
-
-  getSeasons(id) {
-    Vue.http.get(`shows/${id}/seasons?extended=episodes`).then(response => {
-      this.state.seasons = response.json();
-    }, response => {
-      console.error(response.statusText);
+      console.error(response);
     });
   }
 };
