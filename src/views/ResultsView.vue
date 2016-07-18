3ae2d7f3bffa66bb902973412b34198bbef5f92e
Manuel Seemann <accounts@manuelseemann.de>
2016-07-18 23:13:14 +0200

    Separate of components and views

---
+++ src/views/ResultsView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="resultsView" v-if="state.results.length">
+    <result-item
+      v-for="result in state.results"
+      :result="result.show">
+    </result-item>
+  </div>
+</template>
+
+<script>
+import store from './../store';
+import ResultItem from './../components/ResultItem.vue';
+
+export default {
+  components: {
+    ResultItem
+  },
+  data() {
+    return {
+      state: store.state
+    };
+  },
+  route: {
+    data({to}) {
+      store.search(to.params.query);
+    }
+  }
+};
+</script>
